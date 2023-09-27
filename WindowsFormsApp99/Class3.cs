45c64226232b6df3f20e982ff8e3019b2fa899b4
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-27 10:33:57 +0330

    Add files via upload

---
+++ WindowsFormsApp99/Class3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp99
+{
+    class cTarkib
+    {
+
+        public int n
+        {
+            get;
+            set;
+        }
+
+        public int x
+        {
+            get;
+            set;
+        }
+
+        public int combinition(delFac f)
+        {
+            return (int)(f(n) / (f(x) * f(n - x)));
+        }
+    }
+}
